ae28e95a6a716e98565fdb03996d228bed4c69b8
paulAlexSerban <paul.alex.serban@gmail.com>
2022-07-13 09:21:16 +0300

    ci(test): test

--- scripts/docker-traefik-proxy-stop.bash
+++ scripts/docker-traefik-proxy-stop.bash
@@ -2,7 +2,7 @@
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
 
-echo " 🛑  🐳  STOP Traefik Proxy docker containers"
+echo "🛑  🐳  STOP Traefik Proxy docker containers"
 docker compose --env-file ../config.env \
   --file ../docker/docker-compose.traefik-proxy.yml \
   down --volumes --rmi all
